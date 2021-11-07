e272205ca9de356fd576b4dbb2716d13e20c376e
Brad <brt.jst@gmail.com>
2021-11-07 15:10:55 -0700

    simplify and expand code for first & last name generation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const firstNames = ["Brad", "Zintis", "Joe", "Phoebe", "Giuseppe"];
-const lastNames = ["Jones", "Smith", "Brown", "White", "Johnson"];
+const firstNames = ["Brad", "Zintis", "Joe", "Phoebe", "Giuseppe", "Bilbo", "Bob", "Mahatma"];
+const lastNames = ["Jones", "Smith", "Brown", "White", "Johnson", "Baggins", "Loblaw", "Coat"];
 const globalContainerEl = document.getElementById("containerEl");
 
 const randomAge = () => Math.trunc(Math.random() * 18 + 18);
@@ -11,36 +11,10 @@ document.querySelector(".preview").addEventListener("click", function () {
   let number = Number(document.querySelector(".number").value);
   if (number === 0) return alert("You must selct a number; please try again.");
 
-  const fName = function () {
-    let randomFirst = Math.trunc(Math.random() * 5 + 1);
-    if (randomFirst === 1) {
-      return firstNames[0];
-    } else if (randomFirst === 2) {
-      return firstNames[1];
-    } else if (randomFirst === 3) {
-      return firstNames[2];
-    } else if (randomFirst === 4) {
-      return firstNames[3];
-    } else {
-      return firstNames[4];
-    }
-  };
+  const fName = () => Math.trunc(Math.random() * firstNames.length);
 
-  const lName = function () {
-    let randomLast = Math.trunc(Math.random() * 5 + 1);
-    if (randomLast === 1) {
-      return lastNames[0];
-    } else if (randomLast === 2) {
-      return lastNames[1];
-    } else if (randomLast === 3) {
-      return lastNames[2];
-    } else if (randomLast === 4) {
-      return lastNames[3];
-    } else {
-      return lastNames[4];
-    }
-  };
-  
+  const lName = () => Math.trunc(Math.random() * lastNames.length);
+
   const generateCards = function (number) {
     for (let i = 0; i < number; i++) {
       let containerEl = document.createElement("div");
@@ -48,10 +22,10 @@ document.querySelector(".preview").addEventListener("click", function () {
       let smallCard = document.createElement("form");
       containerEl.append(smallCard);
       let firstName = document.createElement("p");
-      firstName.innerHTML = `First Name: ${fName()} `;
+      firstName.innerHTML = `First Name: ${firstNames[fName()]} `;
       smallCard.append(firstName);
       let lastName = document.createElement("p");
-      lastName.innerHTML = `Last Name: ${lName()} `;
+      lastName.innerHTML = `Last Name: ${lastNames[lName()]} `;
       smallCard.append(lastName);
       let age = document.createElement("p");
       age.innerHTML = `Age: ${randomAge()} `;
@@ -59,7 +33,7 @@ document.querySelector(".preview").addEventListener("click", function () {
       let gpa = document.createElement("p");
       gpa.innerHTML = `GPA: ${randomGPA()} `;
       smallCard.append(gpa);
-      
+
       globalContainerEl.append(smallCard);
     }
   };
