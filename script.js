c95185e9a774f4e7f994289fd132881a85e726b7
Brad <brt.jst@gmail.com>
2021-11-07 18:19:11 -0700

    reject negative number input

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const lastNames = ["Jones", "Smith", "Brown", "White", "Johnson", "Baggins", "Lo
 const globalContainerEl = document.getElementById("containerEl");
 
 const randomAge = () => Math.trunc(Math.random() * 18 + 18);
-const randomGPA = () => Math.trunc(Math.random() * 4 + 1);
+const randomGPA = () => (Math.random() * 4 + 1);
+// const  = rando.toArray.slice(-9);
+console.log(randomGPA);
 
 document.querySelector(".preview").addEventListener("click", function () {
   // clears the created element upon each "click"
   containerEl.innerHTML = "";
   let number = Number(document.querySelector(".number").value);
-  if (number === 0) return alert("You must selct a number; please try again.");
+  if (number <= 0) return alert("You must selct a number greter than 1.");
 
   const fName = () => Math.trunc(Math.random() * firstNames.length);
 
